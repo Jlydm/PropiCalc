1c81c02c06389d98516d71533300d12bc954efe6
Jlydm <juliancito8790@gmail.com>
2024-03-31 14:53:40 -0500

    Evitamos que se creen item duplicados si ya existen

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -2,14 +2,24 @@ import { useState } from "react"
 import type { MenuItem, OrderItem } from "../types"
 
 export default function userOrder() {
-
   const [order, setOrder] = useState<OrderItem[]>([])
 
   const addItem = (item: MenuItem) => {
-
-    const newItem = {...item, quantity: 1}
-    setOrder([...order, newItem])
+    // Verificar si ya existe el item en la lista 
+    const itemExist = order.find(orderItem => orderItem.id === item.id)
+    if(itemExist){
+      const updateOrder = order.map( orderItem => orderItem.id === item.id ? 
+        {...orderItem, quantity: orderItem.quantity +1 } : 
+        orderItem 
+      )
+      setOrder(updateOrder)
+    } else {
+      const newItem = {...item, quantity: 1}
+      setOrder([...order, newItem])
+    }
   }
+
+  console.log(order)
 
   return {
     addItem
